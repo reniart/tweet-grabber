423b60ddf76a2ed24b316fd84f5336100ae91552
Viraj Bane <61726906+reniart@users.noreply.github.com>
2022-07-16 17:46:06 -0400

    connected a discord bot to twitter grabber

--- liked_tweets_grabber.py
+++ liked_tweets_grabber.py
@@ -1,11 +1,23 @@
-#code sampled from TwitterDev github
-
 import requests
 import os
 import json
+import discord
+
+from discord.ext import commands
+from typing import FrozenSet
 
 # setting twitterDev project token
 bearer_token = os.environ.get("BEARER_TOKEN")
+
+# setting up discordBot 
+client = discord.Client()
+bot_id = os.environ.get("TWEET_GRABBER")
+
+#discord command set
+commands: FrozenSet[str] = frozenset((
+    "ping",
+    "grab",
+))
 
 def create_url():
     # additional fields include:
@@ -14,9 +26,9 @@ def create_url():
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    tweet_fields = "tweet.fields=lang,author_id,created_at,geo"
+    tweet_fields = "tweet.fields="
     # additional ids can be added with comma spacing
-    id = "2973885323"
+    id = os.environ.get("TWITTER_ID")
 
     url = "https://api.twitter.com/2/users/{}/liked_tweets".format(id)
     return url, tweet_fields
@@ -39,7 +51,7 @@ def connect_endpoint(url, tweet_fields):
         )
     return response.json()
 
-def main():
+def grab_tweets():
     url, tweet_fields = create_url()
     json_response = connect_endpoint(url, tweet_fields)
 
@@ -47,6 +59,32 @@ def main():
     grabbed_tweets.write(json.dumps(json_response, indent=4, sort_keys=True))
     grabbed_tweets.close()
 
-if __name__ == "__main__":
-    main()
+@client.event
+async def on_ready():
+    print("Bot is logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(msg: discord.Message):
+    if msg.author == client.user:
+        return
+
+    if msg.content[0] == "!" and len(msg.content) > 1:
+        args: List[str] = msg.content[1:].lower().split()
+
+        if args[0] not in commands:
+            await msg.channel.send("Unknown command")
+            return
+        
+        if args[0] == "ping":
+            await msg.channel.send(f'Pong! {round(client.latency * 1000)}ms')
+
+        if args[0] == "grab":
+            grab_tweets()
+            await msg.channel.send("Tweets grabbed")
+
+
+
+client.run(bot_id)
+
+
 
