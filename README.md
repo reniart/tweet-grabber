2594f495c2e1cf3007b043880224d30ac13a80e2
Viraj Bane <61726906+reniart@users.noreply.github.com>
2022-07-16 22:45:49 -0400

    minor updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tweet-Grabber
 Grabs liked tweets from my twitter page and posts them in my discord server
+
+took an embarrasingly long time to finish given how simple it is
--- tweet_grabber_bot.py
+++ tweet_grabber_bot.py
@@ -6,19 +6,12 @@ from discord.ext import commands
 from discord.ext import tasks
 from liked_tweets_grabber import grab_tweets
 from configparser import ConfigParser
-from typing import FrozenSet
 
 # setting up discordBot 
 client = discord.Client()
 conf = ConfigParser(interpolation=None)
 conf.read('config.ini')
 bot_id = conf['token']['bot_token']
-
-#discord command set
-commands: FrozenSet[str] = frozenset((
-    "ping",
-    "grab",
-))
 
 @client.event
 async def on_ready():
@@ -30,17 +23,7 @@ async def on_message(msg: discord.Message):
     if msg.author == client.user:
         return
 
-    if msg.content[0] == "!" and len(msg.content) > 1:
-        args: List[str] = msg.content[1:].lower().split()
-
-        if args[0] not in commands:
-            await msg.channel.send("Unknown command")
-            return
-        
-        if args[0] == "ping":
-            await msg.channel.send(f'Pong! {round(client.latency * 1000)}ms') 
-
-@tasks.loop(minutes=1.0)
+@tasks.loop(hours=6.0)
 async def send_new_tweets():
     channel = client.get_channel(998034366737436692)
     #grabbing id of the newest tweet from the last grab cycle
@@ -62,8 +45,6 @@ async def send_new_tweets():
 
             else:
                 await channel.send(f'https://twitter.com/twitter/status/{tweet["id"]}')
-
-    await channel.send(f'in {round(client.latency * 1000)}ms')
 
 
 
